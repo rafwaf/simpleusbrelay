5bd15f4350c15e4b4789072e7dcd6ee4cbca8d83
rafwaf <rafwaf0101@gmail.com>
2021-03-18 18:24:53 -0700

    changed array to relay

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='python-simple-usbrelay',
     url='https://github.com/patrickjahns/simpleusbrelay',
     version='0.1',
-    packages=['simpleusbarray'],
+    packages=['simpleusbrelay'],
     author='Patrick Jahns',
     author_email='patrick.jahns@gmail.com',
     license='MIT',
